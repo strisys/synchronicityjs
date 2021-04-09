1c065e161338d7be24bef95bbc528fa47af08ef8
Stephen Trudel <github@strisys.com>
2021-04-09 15:14:13 -0400

    publish missing types

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -60,4 +60,6 @@ export { QueryTypeCode,
          SearchResult,
          SearchResultPage,
          SearchSuggestionResult,
-         SearchSuggestionResultPage } from './search';
+         SearchSuggestionResultPage,
+         SearchQueryAndPivotResult,
+         SearchQueryAndPivotService } from './search';
